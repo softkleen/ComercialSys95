8cc672b7f8ec6a1d37fc8c6eef45865bbd316f0c
wellington.vsantos4 <wellington.vsantos4@ITQ0675988W10-1>
2024-05-17 08:42:36 -0300

    Adicionar arquivos de projeto.

---
+++ ComercialSys/FrmPrincipal.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ComercialSys
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            cadastrosToolStripMenuItem = new ToolStripMenuItem();
+            cToolStripMenuItem = new ToolStripMenuItem();
+            novoToolStripMenuItem = new ToolStripMenuItem();
+            consultarToolStripMenuItem = new ToolStripMenuItem();
+            produtoToolStripMenuItem = new ToolStripMenuItem();
+            novoToolStripMenuItem1 = new ToolStripMenuItem();
+            consultarToolStripMenuItem1 = new ToolStripMenuItem();
+            operaçãoToolStripMenuItem = new ToolStripMenuItem();
+            pedidoToolStripMenuItem = new ToolStripMenuItem();
+            novoToolStripMenuItem2 = new ToolStripMenuItem();
+            buscarToolStripMenuItem = new ToolStripMenuItem();
+            históricoToolStripMenuItem = new ToolStripMenuItem();
+            movimentoToolStripMenuItem = new ToolStripMenuItem();
+            caixaToolStripMenuItem = new ToolStripMenuItem();
+            usuárioToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { cadastrosToolStripMenuItem, operaçãoToolStripMenuItem, movimentoToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            cadastrosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { cToolStripMenuItem, produtoToolStripMenuItem, usuárioToolStripMenuItem });
+            cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            cadastrosToolStripMenuItem.Size = new Size(71, 20);
+            cadastrosToolStripMenuItem.Text = "&Cadastros";
+            // 
+            // cToolStripMenuItem
+            // 
+            cToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoToolStripMenuItem, consultarToolStripMenuItem });
+            cToolStripMenuItem.Image = Properties.Resources.User;
+            cToolStripMenuItem.Name = "cToolStripMenuItem";
+            cToolStripMenuItem.Size = new Size(180, 22);
+            cToolStripMenuItem.Text = "C&liente";
+            // 
+            // novoToolStripMenuItem
+            // 
+            novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            novoToolStripMenuItem.Size = new Size(125, 22);
+            novoToolStripMenuItem.Text = "&Novo";
+            novoToolStripMenuItem.Click += novoToolStripMenuItem_Click;
+            // 
+            // consultarToolStripMenuItem
+            // 
+            consultarToolStripMenuItem.Name = "consultarToolStripMenuItem";
+            consultarToolStripMenuItem.Size = new Size(125, 22);
+            consultarToolStripMenuItem.Text = "&Consultar";
+            // 
+            // produtoToolStripMenuItem
+            // 
+            produtoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoToolStripMenuItem1, consultarToolStripMenuItem1 });
+            produtoToolStripMenuItem.Image = Properties.Resources.chair_256;
+            produtoToolStripMenuItem.Name = "produtoToolStripMenuItem";
+            produtoToolStripMenuItem.Size = new Size(180, 22);
+            produtoToolStripMenuItem.Text = "&Produto";
+            // 
+            // novoToolStripMenuItem1
+            // 
+            novoToolStripMenuItem1.Name = "novoToolStripMenuItem1";
+            novoToolStripMenuItem1.Size = new Size(125, 22);
+            novoToolStripMenuItem1.Text = "&Novo";
+            // 
+            // consultarToolStripMenuItem1
+            // 
+            consultarToolStripMenuItem1.Name = "consultarToolStripMenuItem1";
+            consultarToolStripMenuItem1.Size = new Size(125, 22);
+            consultarToolStripMenuItem1.Text = "&Consultar";
+            // 
+            // operaçãoToolStripMenuItem
+            // 
+            operaçãoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { pedidoToolStripMenuItem });
+            operaçãoToolStripMenuItem.Name = "operaçãoToolStripMenuItem";
+            operaçãoToolStripMenuItem.Size = new Size(70, 20);
+            operaçãoToolStripMenuItem.Text = "&Operação";
+            // 
+            // pedidoToolStripMenuItem
+            // 
+            pedidoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { novoToolStripMenuItem2, buscarToolStripMenuItem, históricoToolStripMenuItem });
+            pedidoToolStripMenuItem.Name = "pedidoToolStripMenuItem";
+            pedidoToolStripMenuItem.Size = new Size(111, 22);
+            pedidoToolStripMenuItem.Text = "&Pedido";
+            // 
+            // novoToolStripMenuItem2
+            // 
+            novoToolStripMenuItem2.Name = "novoToolStripMenuItem2";
+            novoToolStripMenuItem2.Size = new Size(122, 22);
+            novoToolStripMenuItem2.Text = "&Novo";
+            // 
+            // buscarToolStripMenuItem
+            // 
+            buscarToolStripMenuItem.Name = "buscarToolStripMenuItem";
+            buscarToolStripMenuItem.Size = new Size(122, 22);
+            buscarToolStripMenuItem.Text = "&Buscar";
+            // 
+            // históricoToolStripMenuItem
+            // 
+            históricoToolStripMenuItem.Name = "históricoToolStripMenuItem";
+            históricoToolStripMenuItem.Size = new Size(122, 22);
+            históricoToolStripMenuItem.Text = "&Histórico";
+            // 
+            // movimentoToolStripMenuItem
+            // 
+            movimentoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { caixaToolStripMenuItem });
+            movimentoToolStripMenuItem.Name = "movimentoToolStripMenuItem";
+            movimentoToolStripMenuItem.Size = new Size(81, 20);
+            movimentoToolStripMenuItem.Text = "&Movimento";
+            // 
+            // caixaToolStripMenuItem
+            // 
+            caixaToolStripMenuItem.Image = Properties.Resources.caixareg_gif;
+            caixaToolStripMenuItem.Name = "caixaToolStripMenuItem";
+            caixaToolStripMenuItem.Size = new Size(103, 22);
+            caixaToolStripMenuItem.Text = "&Caixa";
+            // 
+            // usuárioToolStripMenuItem
+            // 
+            usuárioToolStripMenuItem.Name = "usuárioToolStripMenuItem";
+            usuárioToolStripMenuItem.Size = new Size(180, 22);
+            usuárioToolStripMenuItem.Text = "&Usuário";
+            usuárioToolStripMenuItem.Click += usuárioToolStripMenuItem_Click;
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "FrmPrincipal";
+            Text = "ComercialSys 1.0 - Sistema de Administração Comercial";
+            WindowState = FormWindowState.Maximized;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem cadastrosToolStripMenuItem;
+        private ToolStripMenuItem cToolStripMenuItem;
+        private ToolStripMenuItem novoToolStripMenuItem;
+        private ToolStripMenuItem consultarToolStripMenuItem;
+        private ToolStripMenuItem produtoToolStripMenuItem;
+        private ToolStripMenuItem novoToolStripMenuItem1;
+        private ToolStripMenuItem consultarToolStripMenuItem1;
+        private ToolStripMenuItem operaçãoToolStripMenuItem;
+        private ToolStripMenuItem pedidoToolStripMenuItem;
+        private ToolStripMenuItem novoToolStripMenuItem2;
+        private ToolStripMenuItem buscarToolStripMenuItem;
+        private ToolStripMenuItem históricoToolStripMenuItem;
+        private ToolStripMenuItem movimentoToolStripMenuItem;
+        private ToolStripMenuItem caixaToolStripMenuItem;
+        private ToolStripMenuItem usuárioToolStripMenuItem;
+    }
+}
---
+++ ComercialSys/FrmPrincipal.cs
@@ -0,0 +1,26 @@
+namespace ComercialSys
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void novoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmCliente frmCliente = new FrmCliente();
+            frmCliente.MdiParent = this;
+            frmCliente.StartPosition = FormStartPosition.CenterScreen;
+            frmCliente.Show();
+
+        }
+
+        private void usuárioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmUsuario frmUsuario = new FrmUsuario();
+            frmUsuario.MdiParent = this;
+            frmUsuario.Show();
+        }
+    }
+}
