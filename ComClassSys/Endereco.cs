8566a01fea89d972da6fc5c307be67d3a0931ee3
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2024-05-28 09:16:09 -0300

    cliente e endereco completos

--- ComClassSys/Endereco.cs
+++ ComClassSys/Endereco.cs
@@ -13,16 +13,17 @@ namespace ComClassSys
 
         public int Id { get; set; }
         public int ClienteId { get; set; }
-        public string Cep { get; set; }
-        public string Logradouro { get; set; }
-        public string Numero { get; set; }
-        public string Complemento { get; set; }
-        public string Bairro { get; set; }
-        public string Cidade { get; set; }
-        public string Uf { get; set; }
-        public string TipoEndereco { get; set; }
+        public string? Cep { get; set; }
+        public string? Logradouro { get; set; }
+        public string? Numero { get; set; }
+        public string? Complemento { get; set; }
+        public string? Bairro { get; set; }
+        public string? Cidade { get; set; }
+        public string? Uf { get; set; }
+        public string? TipoEndereco { get; set; }
         public Endereco()
         {
+
         }
 
         public Endereco(int id, int clienteId, string cep, string logradouro, string numero, string complemento, string bairro, string cidade, string uf, string tipoEndereco)
@@ -89,7 +90,25 @@ namespace ComClassSys
         public static Endereco ObterPorId(int id) 
         {
             Endereco endereco = new();
-          
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"select * from enderecos where id = {id}";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                endereco = new(
+                        dr.GetInt32(0),
+                        dr.GetInt32(1),
+                        dr.GetString(2),
+                        dr.GetString(3),
+                        dr.GetString(4),
+                        dr.GetString(5),
+                        dr.GetString(6),
+                        dr.GetString(7),
+                        dr.GetString(8),
+                        dr.GetString(9)
+                    );
+            }
             return endereco;
         }
         public static List<Endereco> ObterListaPorCliente(int clienteId)
@@ -98,7 +117,24 @@ namespace ComClassSys
             var cmd = Banco.Abrir();
             cmd.CommandType = CommandType.Text;
             cmd.CommandText = $"select * from enderecos where cliente_id = {clienteId}";
-
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                enderecos.Add(
+                    new(
+                        dr.GetInt32(0), 
+                        dr.GetInt32(1),
+                        dr.GetString(2),
+                        dr.GetString(3),
+                        dr.GetString(4),
+                        dr.GetString(5),
+                        dr.GetString(6),
+                        dr.GetString(7),
+                        dr.GetString(8),
+                        dr.GetString(9)
+                        )
+                    );
+            }
             return enderecos;
         }
     }
